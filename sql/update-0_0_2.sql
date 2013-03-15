cc07177aa01a73e8abd4783356485a04a1aa540b
sdengineering77 <sdengineering77@gmail.com>
2013-03-15 23:20:36 +0100

    table name case sensitive in linux log4j set to info

--- sql/update-0_0_2.sql
+++ sql/update-0_0_2.sql
@@ -1,12 +1,12 @@
 -- Real life URLs can be up to 1024 chars if I'm not mistaken
-alter table video change imageUrl imageUrl varchar(1024) DEFAULT NULL COMMENT 'URL of preview image';
-alter table video change sourceUrl sourceUrl varchar(1024) DEFAULT NULL COMMENT 'For playerType JW';
+alter table Video change imageUrl imageUrl varchar(1024) DEFAULT NULL COMMENT 'URL of preview image';
+alter table Video change sourceUrl sourceUrl varchar(1024) DEFAULT NULL COMMENT 'For playerType JW';
 -- index is limited to first 255 characters; more than that isn't possible
 alter table Video add constraint unique index idx_sourceUrl (sourceUrl(255));
 -- user can now be admin
-alter table user add column isAdmin tinyint(1) null;
+alter table User add column isAdmin tinyint(1) null;
 -- an admin user
-insert into user
+insert into User
 (creationDate, email, name, password, dateOfBirth, usernameFacebook, usernameHyves, usernameTwitter, gender, isAdmin)
 values
 ('2013-03-14 00:00:00', 'maintenance@beeldengeluid.nl', 'maintenance', '4ubpKwX8/o2vt/AHLpZY8KF9E16jEmAwObYA/N/huuv8Kou6T7mLwF77JyDir8gg', null, null, null, null, null, 1);
