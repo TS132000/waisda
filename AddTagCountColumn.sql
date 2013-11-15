dfafcc778f4133cab3065d526239c07acf0c19ab
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2013-11-15 14:06:05 +0100

    Removed old/duplicate SQL scripts

--- AddTagCountColumn.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `manbijthond`.`game` ADD COLUMN `countExistingVideoTags` INT NOT NULL AFTER `video_id`;
--- MakeParticipantData.sql
+++
@@ -1,6 +0,0 @@
-insert into Participant (user_id, game_id, joinedOn)
-select owner_id, game_id, min(creationDate)
-from TagEntry
-group by owner_id, game_id
-order by creationDate asc
-
